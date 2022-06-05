ccba492a2e53622a2475e6e8e5b923e2540e5c72
Romanov Oleg <ya.pochta96@gmail.com>
2022-06-05 13:37:17 +0300

    fix

--- config.db.js
+++ config.db.js
@@ -10,9 +10,8 @@ const {Sequelize} = require('sequelize')
 //         port: process.env.DB_PORT
 //     }
 // )
-const urlDB = 'postgres://bmajbdsaywberq:838ea13c70930d863072edc20a1080c223d37ac67ca032980b4826a397352bfd@ec2-3-211-221-185.compute-1.amazonaws.com:5432/d6j14347dtvobd';
 
-module.exports = new Sequelize(urlDB,
+module.exports = new Sequelize(DATABASE_URL,
     {
         dialectOptions: {
             ssl: {
